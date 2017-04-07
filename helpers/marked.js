6017caad9d331e10a86956a3de697618b6b6c68c
stefan judis <stefanjudis@gmail.com>
2017-04-07 15:58:14 +0200

    add fixes for PWA article

--- helpers/marked.js
+++ helpers/marked.js
@@ -6,6 +6,18 @@ renderer.code = ( code, language ) => {
   return `<pre class="code-${ language }"><code>${ hl.highlightAuto( code ).value }</code></pre>`;
 };
 
+renderer.image = ( href, title, text ) => {
+  if( /\.mp4$/.test( href ) ) {
+    return `
+      <video controls preload="metadata">
+        <source src="${ href }" type="video/mp4">
+      </video>
+    `;
+  }
+
+  return `<img src="${ href }" alt="${ text }">`;
+};
+
 module.exports = function (text) {
   return marked(text, { renderer } );
 };
