7a04a30ed2ac88cc27c43c6064b48694feb09e65
stefan judis <stefanjudis@gmail.com>
2017-08-26 00:50:02 +0200

    Flip env config

--- plugins/contentful.js
+++ plugins/contentful.js
@@ -8,14 +8,15 @@ const defaultConfig = {
 module.exports = {
   createClient (config = defaultConfig) {
     const options = {
+      host: 'preview.contentful.com',
       space: config.CTF_SPACE_ID,
-      accessToken: config.CTF_CDA_TOKEN
+      accessToken: config.CTF_CPA_TOKEN
     }
 
     // enable preview environment
-    if (process.env.NODE_ENV !== 'production') {
-      options.host = 'preview.contentful.com'
-      options.accessToken = config.CTF_CPA_TOKEN
+    if (process.env.NODE_ENV === 'production') {
+      options.host = 'cdn.contentful.com'
+      options.accessToken = config.CTF_CDA_TOKEN
     }
 
     return contentful.createClient(options)
