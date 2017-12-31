fb5ef31790ab4a048e0f2519547f0687e8a9c630
stefan judis <stefanjudis@gmail.com>
2017-12-31 11:27:57 +0100

    Fix ssr and client date display

--- components/PrettyDate.vue
+++ components/PrettyDate.vue
@@ -6,7 +6,8 @@
     props: ['date', 'isSmall', 'isArticleHead'],
     computed: {
       readableDate () {
-        return (new Date(this.date)).toDateString()
+        const [, year, month, day] = this.date.match(/(\d{4})-(\d{2})-(\d{2})/)
+        return (new Date(year, +month - 1, day)).toDateString()
       },
       classes () {
         return {
