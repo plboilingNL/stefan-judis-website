b5feb30812df35d821935eb097f29d07239608f1
stefan judis <stefanjudis@gmail.com>
2018-02-21 22:41:43 +0100

    add more logging to csp handler

--- functions/csp-violation.js
+++ functions/csp-violation.js
@@ -1,7 +1,12 @@
 exports.handler = (event, context, callback) => {
+  const mg = new Mailgun('api-key')
   console.log(event)
-  callback(null, {
-    statusCode: 200,
-    body: 'Hello, World'
-  })
+  console.log(context)
+  try {
+    const report = JSON.stringify(JSON.parse(event.bosy), null, 2)
+
+
+  } catch(e) {
+    callback(e)
+  }
 }
