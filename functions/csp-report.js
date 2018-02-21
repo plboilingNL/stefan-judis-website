12d9cbfc0cff7beba5020adbc2315ee50e90800b
stefan judis <stefanjudis@gmail.com>
2018-02-22 00:14:00 +0100

    Clean up CSP handler

--- functions/csp-report.js
+++ functions/csp-report.js
@@ -1,17 +1,14 @@
 exports.handler = (event, context, callback) => {
   if (event.body) {
-    console.log(process.env.MG_TOKEN, process.env.MG_DOMAIN)
-    console.log('*********************************')
-    console.log(context)
     const apiKey = process.env.MG_TOKEN
     const domain = process.env.MG_DOMAIN
     const mailgun = require('mailgun-js')({apiKey, domain})
-    const report = JSON.parse(event.body);
+    const report = JSON.parse(event.body)
 
     var data = {
       from: 'CSP Report Bot <csp@stefanjudis.com>',
       to: 'stefanjudis@gmail.com',
-      subject: `Blocked ${report['csp-report']['document-uri']}`,
+      subject: `Blocked resource on ${report['csp-report']['document-uri']}`,
       text: JSON.stringify(report, null, 2)
     }
 
