b0750c1b1f8efaf9648eae582d432c8ccda8593e
stefan judis <stefanjudis@gmail.com>
2018-03-08 01:00:27 +0100

    Update person with new lazy loading

--- components/Talk.vue
+++ components/Talk.vue
@@ -4,10 +4,10 @@
     <!-- this is container is needed because of a FF bug -->
     <div class="c-tile__image">
       <lazy-image :asset="talk.fields.thumbnail" :ratio="0.5625"></lazy-image>
-        <div class="c-tile__imageActions">
-          <a v-if="talk.fields.slideUrl" :href="talk.fields.slideUrl" class="o-btn o-btn--floating u-marginHorizontalSmall">Slides</a>
-          <a v-if="talk.fields.videoUrl" :href="talk.fields.videoUrl" class="o-btn o-btn--floating u-marginHorizontalSmall">Recording</a>
-        </div>
+      <div class="c-tile__imageActions">
+        <a v-if="talk.fields.slideUrl" :href="talk.fields.slideUrl" class="o-btn o-btn--floating u-marginHorizontalSmall">Slides</a>
+        <a v-if="talk.fields.videoUrl" :href="talk.fields.videoUrl" class="o-btn o-btn--floating u-marginHorizontalSmall">Recording</a>
+      </div>
     </div>
     <h3 class="o-headline-3">{{ talk.fields.title }}</h3>
   </div>
