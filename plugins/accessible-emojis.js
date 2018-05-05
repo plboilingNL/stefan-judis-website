71f471eec8394e8184b7f59a17687a821f2a5861
stefan judis <stefanjudis@gmail.com>
2018-05-05 13:30:28 +0200

    Add missing emoji label

--- plugins/accessible-emojis.js
+++ plugins/accessible-emojis.js
@@ -10,6 +10,7 @@ const EMOJI_LABELS = {
   '128073': 'Hand pointing right',
   '128075': 'Waving hand',
   '128077': 'Thumbs up',
+  '128106': 'Family',
   '128207': 'Straight ruler',
   '128522': 'Smyling face',
   '129315': 'Rolling on the Floor Laughing Emoji'
