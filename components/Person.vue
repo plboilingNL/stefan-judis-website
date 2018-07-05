d3e5ee7c75bd18b71fef7b059b1ede42ee3adf58
stefan judis <stefanjudis@gmail.com>
2018-07-06 00:14:45 +0200

    Improve lighthouse score on home

--- components/Person.vue
+++ components/Person.vue
@@ -2,7 +2,7 @@
   <div class="c-person u-marginTopLarge">
     <div class="c-person__image">
       <div>
-        <lazy-image class="c-person__image" :asset="person.fields.profilePhoto" :ratio="1"></lazy-image>
+        <img class="c-person__image" :src="person.fields.profilePhoto.fields.file.url" :alt="person.fields.profilePhoto.fields.title" />
         <ul class="c-person__social">
           <li>
             <a href="https://twitter.com/stefanjudis" rel="me" aria-label="Follow me on Twitter">
@@ -30,129 +30,127 @@
 </template>
 
 <script>
-  import marked from '~/plugins/marked.js'
-  import Icon from '~/components/Icon.vue'
-  import LazyImage from '~/components/LazyImage.vue'
+import marked from '~/plugins/marked.js';
+import Icon from '~/components/Icon.vue';
 
-  export default {
-    computed: {
-      bio: function () {
-        return marked(this.person.fields.biography)
-      }
-    },
-    components: {
-      Icon,
-      LazyImage
-    },
-    props: ['person']
-  }
+export default {
+  computed: {
+    bio: function() {
+      return marked(this.person.fields.biography);
+    }
+  },
+  components: {
+    Icon
+  },
+  props: ['person']
+};
 </script>
 
 <style lang="scss">
-  .c-person {
-    display: flex;
-    flex-direction: column;
-    align-items: center;
+.c-person {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
 
-    background-color: #fff;
-    box-shadow: var(--shadow-person);
+  background-color: #fff;
+  box-shadow: var(--shadow-person);
+}
+
+@media (min-width: 45.5em) {
+  .c-person {
+    flex-direction: row;
   }
+}
+
+.c-person__details {
+  padding: 1em;
+
+  @media (min-width: 56em) {
+    font-size: 1.125em;
+  }
+}
+
+.c-person__details p {
+  margin-top: 0;
+}
+
+.c-person__image {
+  width: 100%;
+  display: block;
+  overflow: hidden;
+  position: relative;
+
+  border-radius: 0.125em 0.25em 0 0;
+  overflow: hidden;
 
   @media (min-width: 45.5em) {
-    .c-person {
-      flex-direction : row;
-    }
-  }
-
-  .c-person__details {
-    padding: 1em;
-
-    @media (min-width: 56em) {
-      font-size: 1.125em;
-    }
-  }
-
-  .c-person__details p {
-    margin-top: 0;
-  }
-
-  .c-person__image {
-    width: 100%;
-    overflow: hidden;
-    position: relative;
-
-    border-radius: .125em .25em 0 0;
-    overflow: hidden;
-
-    @media (min-width: 45.5em) {
-      margin: 0 1em 0 0;
-      flex: 0 0 50%;
-      flex-direction: row;
-      border-radius: 0;
-    }
-
-
-    @media (min-width: 56em) {
-      flex: 0 0 33.333%;
-    }
-  }
-
-  .c-person__social {
-    position: absolute;
-    bottom: 1em;
-    left: 0;
-    z-index: 0;
-    color: #fff;
-    list-style: none;
-    padding: 0;
-    margin: 0;
-    display: flex;
+    margin: 0 1em 0 0;
+    flex: 0 0 50%;
     flex-direction: row;
-
-    background: #fff;
+    border-radius: 0;
   }
 
-  .c-person__social li {
-    &:nth-child(1) {
-      a:hover {
-        background: var(--blue-dark);
-      }
-    }
-
-    &:nth-child(2) {
-      a:hover {
-        background: var(--green-dark);
-      }
-    }
-
-    &:nth-child(3) {
-      a:hover {
-        background: var(--red-dark);
-      }
-    }
+  @media (min-width: 56em) {
+    flex: 0 0 33.333%;
   }
+}
 
-  .c-person__social li a {
-    display: block;
-    padding: .5em;
+.c-person__social {
+  position: absolute;
+  bottom: 1em;
+  left: 0;
+  z-index: 0;
+  color: #fff;
+  list-style: none;
+  padding: 0;
+  margin: 0;
+  display: flex;
+  flex-direction: row;
 
-    &:hover {
-      background: var(--c-highlight);
+  background: #fff;
+}
 
-      svg {
-        fill: #fff;
-      }
+.c-person__social li {
+  &:nth-child(1) {
+    a:hover {
+      background: var(--blue-dark);
     }
   }
 
-  .c-person__social svg {
-    display: block;
-    width: 1.5em;
-    height: 1.5em;
-
-    left: 0;
-    top: -0.125em;
-
-    fill: var(--dark-grey);
+  &:nth-child(2) {
+    a:hover {
+      background: var(--green-dark);
+    }
   }
+
+  &:nth-child(3) {
+    a:hover {
+      background: var(--red-dark);
+    }
+  }
+}
+
+.c-person__social li a {
+  display: block;
+  padding: 0.5em;
+
+  &:hover {
+    background: var(--c-highlight);
+
+    svg {
+      fill: #fff;
+    }
+  }
+}
+
+.c-person__social svg {
+  display: block;
+  width: 1.5em;
+  height: 1.5em;
+
+  left: 0;
+  top: -0.125em;
+
+  fill: var(--dark-grey);
+}
 </style>
