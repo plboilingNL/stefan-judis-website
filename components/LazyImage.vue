80588e663fdfead1ec38873a45b04cc02b8d966f
stefan judis <stefanjudis@gmail.com>
2018-07-06 00:46:15 +0200

    Fix image loading - now 100!!!

--- components/LazyImage.vue
+++ components/LazyImage.vue
@@ -1,7 +1,8 @@
 <template>
   <figure role="group" class="c-lazyImage" :style="{ paddingTop: `${ratio * 100}%` }">
     <div class="c-lazyImage--sqip" v-html="preview"></div>
-    <img v-if="ready || load" :src="imageSrc" :alt="asset.fields.title">
+    <img v-if="ready && !load" :src="imageSrc" :alt="asset.fields.title">
+    <img v-if="load" :src="asset.fields.file.url" :alt="asset.fields.title">
   </figure>
 </template>
 
