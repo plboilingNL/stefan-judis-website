db9f08d69f98782ff9e4cfed38367df335099012
stefan judis <stefanjudis@gmail.com>
2018-07-11 20:49:21 +0200

    Is this a good idea?

--- components/Navigation.vue
+++ components/Navigation.vue
@@ -11,8 +11,9 @@
         v-for="(item, index) in menu"
         :key="item.label"
         :data-index="index"
+        @mouseenter="prefetchRoute(item.url)"
       >
-        <nuxt-link :to="item.url">
+        <nuxt-link  :to="item.url">
           <Icon v-if="item.icon" :name="item.icon"></Icon>
           {{ item.label }}
         </nuxt-link>
@@ -29,261 +30,273 @@
   </nav>
 </template>
 <script>
-  import Icon from '~/components/Icon.vue'
-  import Container from '~/components/Container.vue'
+import Icon from '~/components/Icon.vue';
+import Container from '~/components/Container.vue';
 
-  export default {
-    mounted () {
-      this.$router.afterEach(_ => this.toggleNav({isOpen: false}))
+export default {
+  mounted() {
+    this.$router.afterEach(_ => this.toggleNav({ isOpen: false }));
 
-      const mql = window.matchMedia('(min-width:38em)')
-      this.showFullNav = mql.matches
+    const mql = window.matchMedia('(min-width:38em)');
+    this.showFullNav = mql.matches;
 
-      mql.addListener(mql => {
-        this.showFullNav = mql.matches
-      })
-    },
-    data () {
-      return {
-        mainNavIsVisible: false,
-        mainNav: [
-          { label: 'Home', url: '/', icon: 'Home' },
-          { label: 'Blog', url: '/blog/', icon: 'Blog' },
-          { label: 'Resources', url: '/resources/', icon: 'Resources' },
-          { label: 'Talks', url: '/talks/', icon: 'Talk' },
-          { label: 'Smalltalk', url: '/smalltalk/', icon: 'Smalltalk' },
-          { label: 'Today I learned', url: '/today-i-learned/', icon: 'Learn' }
-
-        ],
-        reducedNav: [
-          { label: 'Home', url: '/' },
-          { label: 'Blog', url: '/blog/' },
-          { label: 'Resources', url: '/resources' }
-        ],
-        showFullNav: true
-      }
-    },
-    computed: {
-      menu () {
-        return this.mainNavIsVisible || this.showFullNav
-          ? this.mainNav
-          : []
-      }
-    },
-    methods: {
-      toggleNav (options = {}) {
-        // todo focus the first element
-        this.mainNavIsVisible = typeof options.isOpen !== 'undefined'
-          ? options.isOpen
-          : !this.mainNavIsVisible
-      },
-      beforeEnter (el) {
-        el.style.transitionDelay = `${0.05 * (this.menu.length / 2 - el.dataset.index % 3)}s`
-      }
-    },
-    components: {
-      Container,
-      Icon
+    mql.addListener(mql => {
+      this.showFullNav = mql.matches;
+    });
+  },
+  data() {
+    return {
+      mainNavIsVisible: false,
+      mainNav: [
+        { label: 'Home', url: '/', icon: 'Home' },
+        { label: 'Blog', url: '/blog/', icon: 'Blog' },
+        { label: 'Resources', url: '/resources/', icon: 'Resources' },
+        { label: 'Talks', url: '/talks/', icon: 'Talk' },
+        { label: 'Smalltalk', url: '/smalltalk/', icon: 'Smalltalk' },
+        { label: 'Today I learned', url: '/today-i-learned/', icon: 'Learn' }
+      ],
+      reducedNav: [
+        { label: 'Home', url: '/' },
+        { label: 'Blog', url: '/blog/' },
+        { label: 'Resources', url: '/resources' }
+      ],
+      showFullNav: true
+    };
+  },
+  computed: {
+    menu() {
+      return this.mainNavIsVisible || this.showFullNav ? this.mainNav : [];
     }
+  },
+  methods: {
+    toggleNav(options = {}) {
+      // todo focus the first element
+      this.mainNavIsVisible =
+        typeof options.isOpen !== 'undefined'
+          ? options.isOpen
+          : !this.mainNavIsVisible;
+    },
+    beforeEnter(el) {
+      el.style.transitionDelay = `${0.05 *
+        (this.menu.length / 2 - el.dataset.index % 3)}s`;
+    },
+    prefetchRoute(url) {
+      //! todo make me pretty
+      console.log(this.$route.path);
+      if (this.$route.path !== url) {
+        this.$router
+          .getMatchedComponents(url)
+          .forEach(initComponent => initComponent());
+      }
+    }
+  },
+  components: {
+    Container,
+    Icon
   }
+};
 </script>
 
 <style lang="scss">
-  .c-navigation {
-    position : fixed;
-    width  : 100%;
-    bottom : 0;
-    padding : 0;
-    z-index : 2;
-    background: #fff;
-    box-shadow: 0 0 1em #d1d1d1;
-    transform: translateZ(0);
+.c-navigation {
+  position: fixed;
+  width: 100%;
+  bottom: 0;
+  padding: 0;
+  z-index: 2;
+  background: #fff;
+  box-shadow: 0 0 1em #d1d1d1;
+  transform: translateZ(0);
 
-    @media (min-width: 47em) {
-      bottom: auto;
-      top: 0;
-    }
+  @media (min-width: 47em) {
+    bottom: auto;
+    top: 0;
+  }
 
-    &__complete, &__reduced {
-      list-style: none;
+  &__complete,
+  &__reduced {
+    list-style: none;
+    margin: 0;
+    padding: 0;
+
+    display: grid;
+    grid-template-rows: 1fr;
+    grid-gap: 0.5em;
+
+    > li {
       margin: 0;
       padding: 0;
+      text-align: center;
+      align-self: stretch;
+      justify-self: stretch;
+    }
 
-      display: grid;
-      grid-template-rows: 1fr;
-      grid-gap: .5em;
+    a {
+      display: block;
+      text-decoration: none;
 
-      > li {
-        margin: 0;
-        padding: 0;
-        text-align: center;
-        align-self: stretch;
-        justify-self: stretch;
+      &:hover {
+        border-bottom-color: currentColor;
+        outline: none;
       }
 
-      a {
-        display: block;
-        text-decoration: none;
-
-        &:hover {
-          border-bottom-color: currentColor;
-          outline: none;
-        }
-
-        &:focus {
-          outline-color: var(--c-highlight);
-        }
-
-        &.is-active {
-          color: var(--c-highlight);
-
-          path {
-            fill: currentColor;
-          }
-        }
+      &:focus {
+        outline-color: var(--c-highlight);
       }
 
-      svg {
-        display: block;
-        width: 1.5em;
-        margin: 0 auto .5em;
+      &.is-active {
+        color: var(--c-highlight);
 
         path {
-          fill: #666;
-        }
-
-        @media (min-width: 38em) {
-          display: none;
+          fill: currentColor;
         }
       }
     }
 
-    &__complete {
-      position: absolute;
-      width: 100%;
-      min-height: 12em;
-      z-index: 2;
-      bottom: 0;
-      padding: 2em .75em 4em;
-      background: #fff;
-      box-shadow: 0 0 2em #d1d1d1;
-      grid-template-columns: repeat(3, 1fr);
-      grid-template-rows: repeat(2, 4em);
+    svg {
+      display: block;
+      width: 1.5em;
+      margin: 0 auto 0.5em;
 
-      transform: scale(.2, .225) translate(0, 0);
-      transform-origin: bottom right;
-      transition: transform .225s ease-in-out;
-
-      .main-nav-visible & {
-        transform: scale(1) translate(0, 0);
+      path {
+        fill: #666;
       }
 
-      //
-      // mobile nav full
-      //
       @media (min-width: 38em) {
-        position: static;
-        display: flex;
-        justify-content: space-between;
-        transform: scale(1) translate(0);
-        min-height: 0;
-        padding: 0 2em;
-
-        a {
-          padding: .25em 0;
-          margin: .5em 0;
-          border-bottom: .125em solid transparent;
-          &.is-active {
-            border-bottom-color: currentColor;
-          }
-        }
+        display: none;
       }
+    }
+  }
 
-      @media (min-width: 47em) {
-        justify-content: flex-end;
+  &__complete {
+    position: absolute;
+    width: 100%;
+    min-height: 12em;
+    z-index: 2;
+    bottom: 0;
+    padding: 2em 0.75em 4em;
+    background: #fff;
+    box-shadow: 0 0 2em #d1d1d1;
+    grid-template-columns: repeat(3, 1fr);
+    grid-template-rows: repeat(2, 4em);
 
-        > li {
-          margin-left: 2.5em;
-        }
+    transform: scale(0.2, 0.225) translate(0, 0);
+    transform-origin: bottom right;
+    transition: transform 0.225s ease-in-out;
 
-        a {
-          margin: .75em 0;
-          border-bottom: .125em solid transparent;
+    .main-nav-visible & {
+      transform: scale(1) translate(0, 0);
+    }
 
-          &.is-active {
-            border-top-color: transparent;
-            border-bottom-color: currentColor;
-          }
+    //
+    // mobile nav full
+    //
+    @media (min-width: 38em) {
+      position: static;
+      display: flex;
+      justify-content: space-between;
+      transform: scale(1) translate(0);
+      min-height: 0;
+      padding: 0 2em;
+
+      a {
+        padding: 0.25em 0;
+        margin: 0.5em 0;
+        border-bottom: 0.125em solid transparent;
+        &.is-active {
+          border-bottom-color: currentColor;
         }
       }
     }
 
-    &__reduced {
-      padding-right: 20%;
-      grid-template-columns: repeat(3, auto);
-      transition: opacity .25s ease-in-out;
+    @media (min-width: 47em) {
+      justify-content: flex-end;
 
-      .main-nav-visible & {
-        opacity: 0;
+      > li {
+        margin-left: 2.5em;
       }
 
       a {
-        padding: .5em 0;
-        margin: .25em 0;
+        margin: 0.75em 0;
+        border-bottom: 0.125em solid transparent;
 
         &.is-active {
-          box-shadow: 0 .675em 0 0 currentColor;
+          border-top-color: transparent;
+          border-bottom-color: currentColor;
         }
       }
-
-      //
-      // desktop nav
-      //
-      @media (min-width: 38em) {
-        display: none;
-      }
-    }
-
-    &__toggle {
-      position:absolute;
-      z-index: 3;
-      right: 0;
-      bottom: 0;
-      display: block;
-      width: 20%;
-      height: 100%;
-      font-size: 1em;
-      background: #fff;
-      border: none;
-      color: currentColor;
-
-      //
-      // desktop nav
-      //
-      @media (min-width: 38em) {
-        display: none;
-      }
     }
   }
 
-  .staggered-item, .staggered-item-leave {
-    opacity: 1;
-    transform: translate(0);
+  &__reduced {
+    padding-right: 20%;
+    grid-template-columns: repeat(3, auto);
+    transition: opacity 0.25s ease-in-out;
+
+    .main-nav-visible & {
+      opacity: 0;
+    }
+
+    a {
+      padding: 0.5em 0;
+      margin: 0.25em 0;
+
+      &.is-active {
+        box-shadow: 0 0.675em 0 0 currentColor;
+      }
+    }
+
+    //
+    // desktop nav
+    //
+    @media (min-width: 38em) {
+      display: none;
+    }
   }
 
-  .staggered-item-leave {
-    transition-delay: .25s;
-  }
+  &__toggle {
+    position: absolute;
+    z-index: 3;
+    right: 0;
+    bottom: 0;
+    display: block;
+    width: 20%;
+    height: 100%;
+    font-size: 1em;
+    background: #fff;
+    border: none;
+    color: currentColor;
 
-  .staggered-item-enter-active, .staggered-item-leave-active {
-    transition: all .25s;
+    //
+    // desktop nav
+    //
+    @media (min-width: 38em) {
+      display: none;
+    }
   }
+}
 
-  .staggered-item-enter, .staggered-item-leave-to {
-    opacity: 0;
-  }
+.staggered-item,
+.staggered-item-leave {
+  opacity: 1;
+  transform: translate(0);
+}
 
-  .staggered-item-enter {
-    transform: translateY(1em);
-  }
+.staggered-item-leave {
+  transition-delay: 0.25s;
+}
+
+.staggered-item-enter-active,
+.staggered-item-leave-active {
+  transition: all 0.25s;
+}
+
+.staggered-item-enter,
+.staggered-item-leave-to {
+  opacity: 0;
+}
+
+.staggered-item-enter {
+  transform: translateY(1em);
+}
 </style>
