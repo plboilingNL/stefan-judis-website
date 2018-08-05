2db5c0e129b1de864acbc8a33354467b3495fae7
stefan judis <stefanjudis@gmail.com>
2018-08-05 12:59:17 +0200

    Fix lazyimage position bug

--- components/LazyImage.vue
+++ components/LazyImage.vue
@@ -80,9 +80,9 @@ export default {
   img {
     position: absolute;
     top: 0;
-    right: 0;
-    bottom: 0;
     left: 0;
+    width: 100%;
+    height: 100%;
 
     animation: 0.75s fadeIn;
   }
