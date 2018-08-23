f441e1bb32724ac404076212c21b96d839e43f37
stefan judis <stefanjudis@gmail.com>
2018-08-23 14:40:49 +0200

    Remove unused props from pretty date

--- components/PrettyDate.vue
+++ components/PrettyDate.vue
@@ -1,27 +1,22 @@
 <template>
-  <time :datetime="date" :class="classes">{{ readableDate }}</time>
+  <time :datetime="date" class="c-prettyDate">{{ readableDate }}</time>
 </template>
 <script>
-  export default {
-    props: ['date', 'isSmall'],
-    computed: {
-      readableDate () {
-        const [, year, month, day] = this.date.match(/(\d{4})-(\d{2})-(\d{2})/)
-        return (new Date(year, +month - 1, day)).toDateString()
-      },
-      classes () {
-        return {
-          'c-prettyDate': true
-        }
-      }
+export default {
+  props: ['date'],
+  computed: {
+    readableDate() {
+      const [, year, month, day] = this.date.match(/(\d{4})-(\d{2})-(\d{2})/);
+      return new Date(year, +month - 1, day).toDateString();
     }
   }
+};
 </script>
 
 <style lang="scss">
-  .c-prettyDate {
-    display: block;
-    font-size: .875em;
-    font-weight: 500;
-  }
+.c-prettyDate {
+  display: block;
+  font-size: 0.875em;
+  font-weight: 500;
+}
 </style>
