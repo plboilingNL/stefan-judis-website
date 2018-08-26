e08bde16cb5c1bbc27e69b5ef0c9d337ffc289ab
stefan judis <stefanjudis@gmail.com>
2018-08-26 23:25:04 +0200

    Netlify fixes for incremental builds

--- scripts/utils/index.js
+++ scripts/utils/index.js
@@ -3,10 +3,11 @@ const isNetlify = _ => !!process.env.DEPLOY_URL;
 
 module.exports = {
   isCodeUpdate: process.env.WEBHOOK_TITLE === 'CODE_UPDATE',
-  isContentUpdate: process.env.WEBHOOK_TITLE === 'CONTENT_UPDATE',
+  // DO YOU NEED THIS? IT'S CURRENTLY NOT USED!
+  isContentUpdate: process.env.WEBHOOK_TITLE === 'CONTENTFUL_UPDATE',
   getCachePath: name =>
     isNetlify()
-      ? path.join('/', 'opt', 'build', 'cache', 'dist')
+      ? path.join('/', 'opt', 'build', 'cache', 'dist', name)
       : path.resolve(__dirname, '..', '..', `.${name}`),
   shouldClearCache: process.env.WEBHOOK_TITLE === 'CLEAR_CUSTOM_CACHE'
 };
