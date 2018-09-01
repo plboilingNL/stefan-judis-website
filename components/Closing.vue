31bf6cf906aaf53d7a6cb4e29ae4e578ba6081fd
stefan judis <stefanjudis@gmail.com>
2018-09-02 01:57:40 +0200

    Update closing line

--- components/Closing.vue
+++ components/Closing.vue
@@ -1,6 +1,6 @@
 <template functional>
   <footer class="c-footer u-textAlignCenter">
-    Made with &nbsp;<span aria-label="love">❤️</span>&nbsp; and &nbsp;<a href="https://www.contentful.com">Contentful</a>,&nbsp;<a href="https://www.nuxtjs.org">Nuxt.js</a>, and&nbsp;<a href="https://www.netlify.com">Netlify</a>
+    Made by&nbsp;<a href="https://twitter.com/stefanjudis/">@stefanjudis</a>&nbsp; with &nbsp;<span aria-label="love">❤️</span>,&nbsp;<a href="https://www.contentful.com">Contentful</a>,&nbsp;<a href="https://www.nuxtjs.org">Nuxt.js</a>, and&nbsp;<a href="https://www.netlify.com">Netlify</a>
   </footer>
 </template>
 
