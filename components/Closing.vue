400e166fe67fa75acd52754154bfc6d306066701
stefan judis <stefanjudis@gmail.com>
2018-09-02 00:19:38 +0200

    Fix emoji spacing

--- components/Closing.vue
+++ components/Closing.vue
@@ -1,6 +1,6 @@
 <template functional>
   <footer class="c-footer u-textAlignCenter">
-    Made with <span aria-label="love">❤️</span> and &nbsp;<a href="https://www.contentful.com">Contentful</a>,&nbsp;<a href="https://www.nuxtjs.org">Nuxt.js</a>, and&nbsp;<a href="https://www.netlify.com">Netlify</a>
+    Made with &nbsp;<span aria-label="love">❤️</span>&nbsp; and &nbsp;<a href="https://www.contentful.com">Contentful</a>,&nbsp;<a href="https://www.nuxtjs.org">Nuxt.js</a>, and&nbsp;<a href="https://www.netlify.com">Netlify</a>
   </footer>
 </template>
 
