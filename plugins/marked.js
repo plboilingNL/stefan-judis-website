0ef0f6453e25e838dcb862aea036e28978035084
stefan judis <stefanjudis@gmail.com>
2019-01-19 17:37:26 +0000

    Link images to src and add a tiny code highlight improvement

--- plugins/marked.js
+++ plugins/marked.js
@@ -1,25 +1,25 @@
-import marked from 'marked'
-import Prism from 'prismjs'
-import {makeEmojisAccessible} from './accessible-emojis.js'
+import marked from 'marked';
+import Prism from 'prismjs';
+import { makeEmojisAccessible } from './accessible-emojis.js';
 
-const renderer = new marked.Renderer()
+const renderer = new marked.Renderer();
 
 renderer.code = (code, language) => {
   return `<pre class="o-code o-code-${language || 'whatevs'}"><code>${
     language ? Prism.highlight(code, Prism.languages[language]) : code
-  }</code></pre>`
-}
+  }</code></pre>`;
+};
 
-renderer.heading = function (text, level) {
-  const escapedText = text.toLowerCase().replace(/[^\w]+/g, '-')
+renderer.heading = function(text, level) {
+  const escapedText = text.toLowerCase().replace(/[^\w]+/g, '-');
 
   return `
     <h${level} class="o-anchorContainer" id="${escapedText}">
       <a href="#${escapedText}" aria-label="Anchor link for '${text}'">#</a>
       ${makeEmojisAccessible(text)}
     </h${level}>
-  `
-}
+  `;
+};
 
 renderer.image = (href, title, text) => {
   if (/\.mp4$/.test(href)) {
@@ -27,16 +27,14 @@ renderer.image = (href, title, text) => {
       <video controls preload="metadata">
         <source src="${href}" type="video/mp4">
       </video>
-    `
+    `;
   }
 
-  return `<img src="${href}" alt="${text}">`
-}
+  return `<a href="${href}"><img src="${href}" alt="${text}"></a>`;
+};
 
-renderer.paragraph = (text) => `<p>${makeEmojisAccessible(text)}</p>\n`
+renderer.paragraph = text => `<p>${makeEmojisAccessible(text)}</p>\n`;
 
-export default function (text) {
-  return text
-    ? marked(text, { renderer })
-    : ''
+export default function(text) {
+  return text ? marked(text, { renderer }) : '';
 }
