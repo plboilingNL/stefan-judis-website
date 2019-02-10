cf392e0e283df0ae873ff75e2f708296c967f1cb
stefan judis <stefanjudis@gmail.com>
2019-02-10 19:10:58 +0100

    Fix preload link elem

--- nuxt.config.js
+++ nuxt.config.js
@@ -31,7 +31,7 @@ const config = {
       { rel: 'preconnect', href: 'https://videos.contentful.com' },
       { rel: 'preconnect', href: 'https://images.contentful.com' },
       { rel: 'preconnect', href: 'https://www.google-analytics.com' },
-      { rel: 'preload', href: '/fancy.css' },
+      { rel: 'preload', as: 'font', href: '/fancy.css' },
       {
         rel: 'alternate',
         type: 'application/rss+xml',
