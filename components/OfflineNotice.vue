5522e11162a8ad9806668265277413339aaf686a
stefan judis <stefanjudis@gmail.com>
2019-02-17 18:42:33 +0100

    Fix offline msg

--- components/OfflineNotice.vue
+++ components/OfflineNotice.vue
@@ -2,11 +2,21 @@
   <div class="c-offlineNotice" role="alert">You're offline...</div>
 </template>
 
-<style>
-  .c-offlineNotice {
-    text-align: center;
-    padding: .5em;
-    margin: 1em 0;
-    background: var(--grey-bright);
+<style lang="postcss">
+.c-offlineNotice {
+  position: absolute;
+  display: grid;
+  place-items: center;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 3.5em;
+  text-align: center;
+  padding: 0.5em;
+  background: var(--grey-bright);
+
+  @media (min-width: 52em) {
+    top: 4em;
   }
+}
 </style>
