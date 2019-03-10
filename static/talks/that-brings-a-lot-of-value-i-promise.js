641d1fedbefc10c24eccbee990fac590f5dbd158
stefan judis <stefanjudis@gmail.com>
2019-03-10 18:53:24 +0100

    Add talk script

---
+++ static/talks/that-brings-a-lot-of-value-i-promise.js
@@ -0,0 +1,9 @@
+const script = document.createElement('script');
+script.onload = function() {
+  for (let i = 0; i < 100; i++) {
+    window.cornify_add();
+  }
+};
+script.src = 'https://www.cornify.com/js/cornify.js';
+
+document.head.appendChild(script);
