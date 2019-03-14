2db16977e83447c0306aed496457baa45161c696
stefan judis <stefanjudis@gmail.com>
2019-03-14 21:37:13 +0100

    Less unicorns are better unicorns

--- static/talks/that-brings-a-lot-of-value-i-promise.js
+++ static/talks/that-brings-a-lot-of-value-i-promise.js
@@ -1,6 +1,6 @@
 const script = document.createElement('script');
 script.onload = function() {
-  for (let i = 0; i < 100; i++) {
+  for (let i = 0; i < 10; i++) {
     window.cornify_add();
   }
 };
