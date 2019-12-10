d11639da7b55477a694f4acecd693134cd01eb1c
stefan judis <stefanjudis@gmail.com>
2019-12-10 18:50:25 +0100

    Make tiny a11y doggo fix

--- components/Closing.vue
+++ components/Closing.vue
@@ -85,6 +85,7 @@
             version="1.1"
             viewBox="0 0 100 100"
             xmlns="http://www.w3.org/2000/svg"
+            aria-hidden="true"
           >
             <g>
               <path
