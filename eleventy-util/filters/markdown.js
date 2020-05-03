5008400d2ae63ab57ec5b59d3315929804190e72
stefan judis <stefanjudis@gmail.com>
2020-05-03 19:17:08 +0200

    Add sqip

--- eleventy-util/filters/markdown.js
+++ eleventy-util/filters/markdown.js
@@ -1,7 +1,29 @@
+const SQIP_DATA = require('../../site/_data/_sqip.json');
 const md = require('markdown-it')({
   html: true,
-  linkify: false
+  linkify: false,
 });
+
+md.renderer.rules.image = function (tokens, idx, options, env, self) {
+  // https://github.com/markdown-it/markdown-it/blob/master/docs/architecture.md#renderer
+  const token = tokens[idx];
+  const aIndex = token.attrIndex('src');
+  const src = token.attrs[aIndex][1];
+  const alt = token.content;
+
+  const [, assetId] = src.match(
+    /(?:\/\/|https:\/\/)(?:images|videos).(?:ctfassets|contentful).(?:net|com)\/.*?\/(.*?)\/.*/
+  );
+
+  let svg = SQIP_DATA[assetId] || '';
+
+  return `
+    <figure class="sqip-image">
+      ${svg}
+      <img src=${src} alt="${alt}" loading="lazy">
+    </figure>
+  `;
+};
 
 md.use(require('markdown-it-prism'));
 md.use(require('markdown-it-toc-and-anchor').default);
@@ -9,6 +31,6 @@ md.use(require('markdown-it-toc-and-anchor').default);
 module.exports = ({ eleventyConfig, filterName }) => {
   eleventyConfig.addNunjucksFilter(
     filterName,
-    value => `<div class="markdown">${md.render(value)}</div>`
+    (value) => `<div class="markdown">${md.render(value)}</div>`
   );
 };
--- scripts/utils/index.js
+++ scripts/utils/index.js
@@ -1,22 +1,19 @@
 const path = require('path');
 const { writeFile } = require('fs-extra');
-const isNetlify = _ => !!process.env.DEPLOY_URL;
+const isNetlify = (_) => !!process.env.DEPLOY_URL;
 const chalk = require('chalk');
 
 const DATA_PATH = path.resolve(__dirname, '..', '..', 'data');
 
 module.exports = {
-  isCodeUpdate: process.env.WEBHOOK_TITLE === 'CODE_UPDATE',
-  // DO YOU NEED THIS? IT'S CURRENTLY NOT USED!
-  isContentUpdate: process.env.WEBHOOK_TITLE === 'CONTENTFUL_UPDATE',
-  getCachePath: name =>
+  getCachePath: (name) =>
     isNetlify()
       ? path.join('/', 'opt', 'build', 'cache', name)
       : path.resolve(__dirname, '..', '..', `.${name}`),
-  logError: text => console.log(chalk.red(`!!! ${text}\n`)),
-  logSuccess: text => console.log(chalk.green(`✔ ${text}\n`)),
-  logSeparator: text => console.log(chalk.yellow(`↻ ${text}...`)),
+  logError: (text) => console.log(chalk.red(`!!! ${text}\n`)),
+  logSuccess: (text) => console.log(chalk.green(`✔ ${text}\n`)),
+  logSeparator: (text) => console.log(chalk.yellow(`↻ ${text}...`)),
   shouldClearCache: process.env.WEBHOOK_TITLE === 'CLEAR_CUSTOM_CACHE',
   writeData: (fileName, content) =>
-    writeFile(path.join(DATA_PATH, fileName), content)
+    writeFile(path.join(DATA_PATH, fileName), content),
 };
