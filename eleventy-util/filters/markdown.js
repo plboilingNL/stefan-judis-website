a148bab1c24b8652a2a08e27fdd3da4d3b3ebd8f
stefan judis <stefanjudis@gmail.com>
2020-05-17 15:59:09 +0200

    Clean up responsive images in markdown

--- eleventy-util/filters/markdown.js
+++ eleventy-util/filters/markdown.js
@@ -27,32 +27,21 @@ md.renderer.rules.image = function (tokens, idx, options, env, self) {
   // because getAssets is only called on this space
   return details
     ? `
-    <figure class="sqip-image" style="padding-top: ${details.ratio}%;">
-      ${svg}
-      <img width="${1000} height="${(1000 * details.ratio) / 100}"
-      srcset="
-        ${src}?fm=jpg&fit=scale&q=75&w=300&h=${Math.floor(
-        (300 * details.ratio) / 100
-      )} 300w,
-        ${src}?fm=jpg&fit=scale&q=75&w=500&h=${Math.floor(
-        (500 * details.ratio) / 100
-      )} 500w,
-        ${src}?fm=jpg&fit=scale&q=75&w=700&h=${Math.floor(
-        (700 * details.ratio) / 100
-      )} 700w,
-        ${src}?fm=jpg&fit=scale&q=75&w=900&h=${Math.floor(
-        (900 * details.ratio) / 100
-      )} 900w,
-        ${src}?fm=jpg&fit=scale&q=75&w=1100&h=${Math.floor(
-        (1100 * details.ratio) / 100
-      )} 1100w
-      "
-      sizes="
-        (max-width: 50em) 98vw,
-        700px"
-      src="${src}" alt="${encode(alt)}" loading="lazy">
-    </figure>
-  `
+      <figure class="sqip-image" style="padding-top: ${details.ratio}%;">
+        ${svg}
+        <img width="${1000} height="${(1000 * details.ratio) / 100}"
+        srcset="${[300, 500, 700, 900, 1100]
+          .map(
+            (size) =>
+              `${src}?fm=jpg&fit=scale&q=75&w=${size}&h=${Math.floor(
+                (size * details.ratio) / 100
+              )} ${size}w`
+          )
+          .join(', ')}"
+        sizes="(max-width: 50em) 98vw, 700px"
+        src="${src}" alt="${encode(alt)}" loading="lazy">
+      </figure>
+    `
     : `<img src="${src}" alt="${encode(alt)}" loading="lazy" />`;
 };
 
